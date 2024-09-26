f896b917b279a047603a0198d3cf84c8b0e84f82
Tom Houlé <tom@tomhoule.com>
2024-09-26 13:21:22 +0200

    dockerfile-

---
+++ product/server.ts
@@ -0,0 +1,26 @@
+import { parse } from "graphql";
+import { buildSubgraphSchema } from "@apollo/subgraph";
+import { createYoga } from "graphql-yoga";
+import schema from "./product.graphql" with { type: "text" };
+
+async function main() {
+  const yoga: any = createYoga({
+    schema: buildSubgraphSchema({
+      typeDefs: parse(schema),
+      resolvers: {},
+    }),
+  });
+
+  const listenHost = "0.0.0.0";
+  const listenPort = 4000;
+
+  Bun.serve({
+    port: listenPort,
+    hostname: listenHost,
+    fetch: yoga,
+  });
+
+  console.log(`Server running on ${listenHost}:${listenPort}`);
+}
+
+main().catch(console.error);
